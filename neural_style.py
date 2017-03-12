55dfdd2a98f86c28f99a7cfcc9d9585955a6164e
RaptimusPrime <vin9jain@yahoo.co.in>
2017-03-12 19:42:16 +0530

    Add argument parser and modularize code

--- neural_style.py
+++ neural_style.py
@@ -1,26 +1,50 @@
 import numpy as np
 import tensorflow as tf
 import scipy.misc
+from argparse import ArgumentParser
 import vgg19
 import losses
 
-contentPath = "examples/willy_wonka.jpg"
-stylePath = "examples/style.jpg"
-outputPath = "examples/output.jpg"
-modelPath = "imagenet-vgg-verydeep-19.mat"
+ITERATIONS = 1000
+LEARNING_RATE = 1e1
+OUTPUT = 'result.jpg'
+MODEL_PATH = 'imagenet-vgg-verydeep-19.mat'
 
 content_layers = ['relu5_2']
 style_layers = ['relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1']
 
-def stylize(model, content_image, style_image, init_image):
+def build_argparser():
+	parser = ArgumentParser()
+
+	parser.add_argument('--content',
+			dest='content',	help='Content Image',
+			type=str, required=True)
+	parser.add_argument('--style',
+			dest='style', help='Style Image',
+			type=str, required=True)
+	parser.add_argument('--output',
+			dest='output', help='Output Image',
+			type=str, default=OUTPUT)
+	parser.add_argument('--iterations',
+			dest='iterations', help='Number of Iterations (default: %(default)s)',
+			type=int, default=ITERATIONS)
+	parser.add_argument('--model',
+			dest='model', help='File containing Model Parameters (default: %(default)s)',
+			type=str, default=MODEL_PATH)
+	parser.add_argument('--learning-rate',
+			dest='learning_rate', help='Learning Rate (default: %(default)s)',
+			type=float, default=LEARNING_RATE)
+	return parser
+
+
+
+def stylize(model, content_image, style_image, init_image,
+			iterations, learning_rate):
 	with tf.Session() as sess:
 		L_content = losses.total_content_loss(sess, model, content_image, content_layers)
 		L_style = losses.total_style_loss(sess, model, style_image, style_layers)
 
 		L_total = L_content + L_style
-
-		learning_rate = 10
-		max_iters = 1000
 
 		optimizer = tf.train.AdamOptimizer(learning_rate)
 
@@ -30,7 +54,7 @@ def stylize(model, content_image, style_image, init_image):
 		sess.run(model['input'].assign(init_image))
 
 		i = 0
-		while (i < max_iters):
+		while (i < iterations):
 			sess.run(train_op)
 			print 'Iteration '+str(i)+'/1000'
 			i += 1
@@ -51,21 +75,33 @@ def postprocess(image, mean):
 	image = np.clip(image, 0, 255).astype('uint8')
 	return image
 
-content_image = scipy.misc.imread(contentPath).astype(np.float32)
-style_image = scipy.misc.imread(stylePath).astype(np.float32)
+def main():
+	parser = build_argparser()
+	args = parser.parse_args()
 
-model, mean = vgg19.build_net(modelPath, content_image)
+	content_image = scipy.misc.imread(args.content).astype(np.float32)
+	style_image = scipy.misc.imread(args.style).astype(np.float32)
 
-shape = content_image.shape
-style_image = scipy.misc.imresize(style_image, shape)
+	model, mean = vgg19.build_net(args.model, content_image)
 
-content_image = preprocess(content_image, mean)
-style_image = preprocess(style_image, mean)
+	shape = content_image.shape
+	style_image = scipy.misc.imresize(style_image, shape)
 
-init_image = content_image
+	content_image = preprocess(content_image, mean)
+	style_image = preprocess(style_image, mean)
 
-output_image = stylize(model, content_image, style_image, init_image)
+	init_image = content_image
 
-output_image = postprocess(output_image, mean)
+	output_image = stylize(model=model, 
+						content_image=content_image,
+						style_image=style_image,
+						init_image=init_image,
+						iterations=args.iterations,
+						learning_rate=args.learning_rate)
 
-scipy.misc.imsave(outputPath, output_image)
+	output_image = postprocess(output_image, mean)
+	scipy.misc.imsave(args.output, output_image)
+	return
+
+if __name__ == '__main__':
+    main()
